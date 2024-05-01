0c1caad7ca261bdd10728253a71123e3c7334b0f
KostasPapd <kostispapd@outlook.com>
2024-05-01 11:40:13 +0100

    f

--- isValid.py
+++ isValid.py
@@ -1,3 +1,18 @@
 # Need for this project:
 # Email validation and verification
 # Student id validation
+# Password length check
+# Answer data validation
+
+def validEmail(email):
+    import re
+
+    regex = re.compile(r'([A-Za-z0-9]+[.-_])*[A-Za-z0-9]+@[A-Za-z0-9]+(\.[A-Z|a-z]{2,})+')
+
+    if re.fullmatch(regex, email):
+        return True
+    else:
+        return False
+
+def verifyEmail(email):
+
