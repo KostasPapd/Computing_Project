c4d07a2b4a71740919b1ed89bb774c9d7a9f1a44
KostasPapd <kostispapd@outlook.com>
2024-05-03 14:32:16 +0100

    f

--- testValidation.py
+++ testValidation.py
@@ -1,9 +1,15 @@
+"""
+Unittest website: https://www.geeksforgeeks.org/unit-testing-python-unittest/
+"""
+
 import unittest
 from isValid import *
 
 class Test(unittest.TestCase):
     def test_email(self):
-        self.assert_(validEmail("ojojbho@jgvu.com"),True)
+        self.assertTrue(validEmail("ojojbho@jgvu.com"))
+    def test_password(self):
+        self.assertTrue(validatePassword("12!bbHhhhs"), True)
 
 
 if __name__ == "__main__":
