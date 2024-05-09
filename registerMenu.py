c12932baec9b94a6b5f76fbd0208f3b9a4ffea42
KostasPapd <kostispapd@outlook.com>
2024-05-09 18:11:12 +0100

    f

--- registerMenu.py
+++ registerMenu.py
@@ -4,30 +4,37 @@ import validateRegisterData
 
 def getVal(nameBox, userBox, emailBox, passBox, repassBox, c, o):
     name = nameBox.get("1.0", "end-1c")
-    # Capitalises the first letter of the first name and surname for the database
-    space = 0
-    for i in range(len(name)):
-        if name[i].isspace():
-            space = i
-    name = name[0].capitalize() + name[1:(space)] + " " + name[space + 1].capitalize() + name[space + 2:]
     username = userBox.get("1.0", "end-1c").strip()
     email = emailBox.get("1.0", "end-1c").strip()
     password = passBox.get("1.0", "end-1c").strip()
     repassword = repassBox.get("1.0", "end-1c").strip()
     school = c.get()
     level = o.get()
-    # PUTS ACCOUNT IN THE DATABASE STUDENT OR TEACHER
-    if school == "School not listed":
-        messagebox.showwarning("School not listed", "You can't create an account because your school isn't"
-                                                    " registered to PROGRAM NAME. Please talk to a teacher if you want "
-                                                    "to register with us")
-        # ADD PROGRAM NAME
+    if (len(name) < 1 or len(username) < 1 or len(email) < 1 or len(password) < 1 or school == "School name" or
+            level == "Level"):
+        messagebox.showwarning("Empty Field", "Please fill out all fields")
     else:
-        if password != repassword:
-            messagebox.showwarning("Passwords don't match", "Passwords don't match. Please try again")
+        # Capitalises the first letter of the first name and surname for the database
+        space = 0
+        for i in range(len(name)):
+            if name[i].isspace():
+                space = i
+        name = name[0].capitalize() + name[1:(space)] + " " + name[space + 1].capitalize() + name[space + 2:]
+
+        # PUTS ACCOUNT IN THE DATABASE STUDENT OR TEACHER
+        if school == "School not listed":
+            messagebox.showwarning("School not listed", "You can't create an account because your school "
+                                                        "isn't registered to PROGRAM NAME. Please talk to a teacher if "
+                                                        "you want to register with us")
+            # ADD PROGRAM NAME
         else:
-            validateRegisterData.checkVal(username, email, password)
-    # Strip gets rid of whitespace, 1.0 and end-1c is where the indexing starts and ends
+            if password != repassword:
+                messagebox.showwarning("Passwords don't match", "Passwords don't match. Please try again")
+            else:
+                validateRegisterData.checkVal(name, username, email, password)
+        # Strip gets rid of whitespace, 1.0 and end-1c is where the indexing starts and ends
+
+
 
 def back(win):
     import createMainMenu
--- validateRegisterData.py
+++ validateRegisterData.py
@@ -2,16 +2,19 @@ from tkinter import messagebox
 from isValid import *
 
 
-def checkVal(username, email, password):
-    if validateUsername(username) == False:
-        # Add username verification
-        messagebox.showwarning("Invalid Username", "This username is invalid or already taken")
-    elif validEmail(email) == False:
-        if verifyEmail(email) == False:
-            messagebox.showwarning("Invalid email", "The email you have entered is invalid")
-    elif validatePassword(password) == False:
-        messagebox.showwarning("Invalid password", "Your password must include: an uppercase letter, a "
-                                                   "lowercase letter, a number, a special character (!@_&) and more "
-                                                   "than 8 characters")
+def checkVal(name, username, email, password):
+    if len(name) < 1 or len(username) < 1 or len(email) < 1 or len(password) < 1:
+        messagebox.showwarning("Empty Field", "Please fill out all fields")
     else:
-        print("Approved")
+        if validateUsername(username) == False:
+            # Add username verification
+            messagebox.showwarning("Invalid Username", "This username is invalid or already taken")
+        elif validEmail(email) == False:
+            if verifyEmail(email) == False:
+                messagebox.showwarning("Invalid email", "The email you have entered is invalid")
+        elif validatePassword(password) == False:
+            messagebox.showwarning("Invalid password", "Your password must include: an uppercase letter, a "
+                                                       "lowercase letter, a number, a special character (!@_&) and more "
+                                                       "than 8 characters")
+        else:
+            print("Approved")
