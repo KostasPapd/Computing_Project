7dca89b26d886682cbbe37228c44be37763cfc46
KostasPapd <kostispapd@outlook.com>
2024-05-10 09:03:09 +0100

    f

--- validateRegisterData.py
+++ validateRegisterData.py
@@ -1,7 +1,7 @@
 from tkinter import messagebox
 from isValid import *
 
-
+#CHANGE TO VERIFY EMAIL
 def checkVal(name, username, email, password):
     if len(name) < 1 or len(username) < 1 or len(email) < 1 or len(password) < 1:
         messagebox.showwarning("Empty Field", "Please fill out all fields")
@@ -9,12 +9,12 @@ def checkVal(name, username, email, password):
         if validateUsername(username) == False:
             # Add username verification
             messagebox.showwarning("Invalid Username", "This username is invalid or already taken")
-        elif validEmail(email) == False:
+        elif validEmail(email) == True:
             if verifyEmail(email) == False:
                 messagebox.showwarning("Invalid email", "The email you have entered is invalid")
         elif validatePassword(password) == False:
             messagebox.showwarning("Invalid password", "Your password must include: an uppercase letter, a "
-                                                       "lowercase letter, a number, a special character (!@_&) and more "
-                                                       "than 8 characters")
+                                                       "lowercase letter, a number, a special character (!@_&) and more"
+                                                       " than 8 characters")
         else:
             print("Approved")
