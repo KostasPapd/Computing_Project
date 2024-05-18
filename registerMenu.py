599461e96853513d0411e29a23285b2e6081abdd
KostasPapd <kostispapd@outlook.com>
2024-05-18 17:32:06 +0100

    f

--- registerMenu.py
+++ registerMenu.py
@@ -74,21 +74,21 @@ def createBox():
 # Username label and text box
     userLabel = Label(window, text="Username:")
     userLabel.config(font=("Arial", 14))
-    userLabel.place(x=82, y=93)
+    userLabel.place(relx=0.145, rely=0.22, relheight=0.1, relwidth=0.2)
     userBox = Text(window, height=1, width=30)
     userBox.place(relx=0.37, rely=0.25, relheight=0.05, relwidth=0.48)
 
 # Email label and text box
     emailLabel = Label(window, text="Email Address:")
     emailLabel.config(font=("Arial", 14))
-    emailLabel.place(x=45, y=130)
+    emailLabel.place(relx=0.06, rely=0.315, relheight=0.1, relwidth=0.3)
     emailBox = Text(window, height=1, width=30)
     emailBox.place(relx=0.37, rely=0.34, relheight=0.05, relwidth=0.48)
 
 # Password label and text box
     passLabel = Label(window, text="Password:")
     passLabel.config(font=("Arial", 14))
-    passLabel.place(x=85, y=170)
+    passLabel.place(relx=0.15, rely=0.415, relheight=0.1, relwidth=0.2)
     # Makes whatever is entered into bullet points
     passBox = Entry(window, textvariable=password_var, font=('Arial', 12), show='•', width=27)
     passBox.place(relx=0.37, rely=0.44, relheight=0.05, relwidth=0.48)
@@ -96,43 +96,43 @@ def createBox():
 # Re-type label and text box
     repassLabel = Label(window, text="Re-type Password:")
     repassLabel.config(font=("Arial", 14))
-    repassLabel.place(x=15, y=210)
+    repassLabel.place(relx=0.02, rely=0.51, relheight=0.1, relwidth=0.32)
     repassBox = Entry(window, textvariable=repassword_var, font=('Arial', 12), show='•', width=27)
     repassBox.place(relx=0.37, rely=0.54, relheight=0.05, relwidth=0.48)
 
 # School label
     schoolLabel = Label(window, text="School:")
     schoolLabel.config(font=("Arial", 14))
-    schoolLabel.place(x=110, y=250)
+    schoolLabel.place(relx=0.18, rely=0.61, relheight=0.1, relwidth=0.2)
 
 # Drop-down menu for school
     options = ["City of Stoke-on-Trent Sixth Form College", "School not listed"]
     c = StringVar()
     c.set("School name")
     schoolMenu = OptionMenu(window, c, *options)
-    schoolMenu.place(x=182, y=250)
+    schoolMenu.place(relx=0.36, rely=0.62, relheight=0.08, relwidth=0.22)
 
 
 # Label for level
     levelLabel = Label(window, text="I am a:")
     levelLabel.config(font=("Arial", 14))
-    levelLabel.place(x=115, y=290)
+    levelLabel.place(relx=0.21, rely=0.72, relheight=0.08, relwidth=0.15)
 
 # Drop-down menu for level of account
     levelOpt = ["Teacher", "Student"]
     o = StringVar()
     o.set("Level")
     levelMenu = OptionMenu(window, o, *levelOpt)
-    levelMenu.place(x=182, y=290)
+    levelMenu.place(relx=0.36, rely=0.72, relheight=0.08, relwidth=0.15)
 
 # Show password buttons
     showPassImg = PhotoImage(file="Pictures/showPassword.png")
     showPassImg = showPassImg.subsample(15, 15)
     showPass = Button(window, image=showPassImg, command=lambda: togglePass(passBox))
-    showPass.place(x=440, y=171)
+    showPass.place(relx=0.87, rely=0.43, relheight=0.07, relwidth=0.1)
 
     showRePass = Button(window, image=showPassImg, command=lambda: togglePass(repassBox))
-    showRePass.place(x=440, y=213)
+    showRePass.place(relx=0.87, rely=0.53, relheight=0.07, relwidth=0.1)
 
     nameBox.focus()
     window.mainloop()
