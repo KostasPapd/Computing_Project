f1dc73ee453025fd521acdcf82c0b248a39f824c
KostasPapd <kostispapd@outlook.com>
2024-05-21 11:43:24 +0100

    f

--- connectSupabase.py
+++ connectSupabase.py
@@ -1,1 +1,20 @@
 import psycopg2
+import os
+from dotenv import load_dotenv, dotenv_values
+load_dotenv()
+connector_key = os.getenv("DB_KEY")
+
+try:
+    conn = psycopg2.connect(connector_key)
+    print("Connected to the database")
+except Exception as e:
+    print("Connection failed")
+    print("Error: ", e)
+
+
+#with psycopg2.connect(connector_key) as conn:
+#    with conn.cursor() as cursor:
+#        cursor.execute("SELECT * FROM AdminAcc")
+#        cursor.fetchone()
+#        for record in cursor:
+#            print(record)
