dc5199db74d3ee5466f12680aef215e1bf6f16b4
KostasPapd <kostispapd@outlook.com>
2024-06-11 09:27:34 +0100

    f

---
+++ Testing/SQLfunctions_testting.py
@@ -0,0 +1,19 @@
+import unittest
+import SQLfunctions
+
+class Validate_SQLfunctions(unittest.TestCase):
+    def testCheckEmail(self):
+        data = "kostispapd@outlook.com"
+        result = SQLfunctions.checkEmail(data)
+        expected = False
+        self.assertEqual(expected, result)
+
+    def testCheckLogIn(self):
+        user = "admin_kostas"
+        password = "UnittestPassword"
+        result = SQLfunctions.checkLogIn(user, password)
+        expected = None
+        self.assertEqual(expected, result)
+
+if __name__ == "__main__":
+    unittest.main()
