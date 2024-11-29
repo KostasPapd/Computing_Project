3da0a4c795707f4676e731c3fb45648e5409c2db
KostasPapd <kostispapd@outlook.com>
2024-11-29 10:08:33 +0000

    f

--- 2FA.py
+++ 2FA.py
@@ -1,3 +1,27 @@
 """
 https://www.geeksforgeeks.org/two-factor-authentication-using-google-authenticator-in-python/
 """
+
+import time
+import pyotp
+import qrcode
+
+key = "GeeksforGeeksIsBestForEverything"
+
+uri = pyotp.totp.TOTP(key).provisioning_uri(
+	name='Test',
+	issuer_name='ThePhysicsLab')
+
+print(uri)
+
+
+# Qr code generation step
+qrcode.make(uri).save("qr.png")
+
+"""Verifying stage starts"""
+
+totp = pyotp.TOTP(key)
+
+# verifying the code
+while True:
+    print(totp.verify(input(("Enter the Code : "))))
